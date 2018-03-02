5649532c2070cc31a0f640578ee32f57dbc77e39
Tom Bloom <thomasbloom25@gmail.com>
2018-03-02 16:42:07 +0000

    Initial Player Movement

--- Colour Switch Game/Assets/Art/Tiled/TileSets/Colour_Switch_Level.tsx
+++ Colour Switch Game/Assets/Art/Tiled/TileSets/Colour_Switch_Level.tsx
@@ -3,7 +3,7 @@
  <image source="../../Export/GameObjects/Level/Floor_Tile.png" width="65" height="65"/>
  <tile id="0">
   <objectgroup draworder="index">
-   <object id="5" x="0.664611" y="0.664611" width="62.3405" height="62.3405"/>
+   <object id="8" x="-0.0344485" y="0" width="64.1961" height="63.8034"/>
   </objectgroup>
  </tile>
 </tileset>
--- Colour Switch Game/Assets/Scripts/GameObjects/Player/PlayerController.cs
+++ Colour Switch Game/Assets/Scripts/GameObjects/Player/PlayerController.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[System.Serializable]
+public class GroundChecks
+{
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float groundCheckRadius;
+    public bool isGrounded;
+}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[System.Serializable]
+public class Movement
+{
+    public float moveSpeed;
+    public float jumpHeight;
+}
+
+public class PlayerController : MonoBehaviour {
+    public GroundChecks gc;
+    public Movement movement;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if player is grounded
+        gc.isGrounded = Physics2D.OverlapCircle(gc.groundCheck.position, gc.groundCheckRadius,
+            gc.whatIsGround);
+
+        // Get player input -> Horizontal movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Apply horizontal movement
+        rb.velocity = new Vector2(moveHorizontal * movement.moveSpeed, rb.velocity.y);
+
+        // Jump
+        if(gc.isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(rb.velocity.x, movement.jumpHeight));
+    }
 }
