d2e1a71727ce75929654a1f303ef993d2196da01
Tom Bloom <thomasbloom25@gmail.com>
2018-03-02 15:41:18 +0000

    Initial Artwork

---
+++ Colour Switch Game/Assets/Art/Tiled/TileSets/Colour_Switch_Level.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Colour_Switch_Level" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../Export/GameObjects/Level/Floor_Tile.png" width="65" height="65"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="5" x="0.664611" y="0.664611" width="62.3405" height="62.3405"/>
+  </objectgroup>
+ </tile>
+</tileset>
